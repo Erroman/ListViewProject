40e5520cfb65cf4a4cd476a53d4421cb75ee4c66
Vladimir Romanovsky <vladimir_roman@mail.ru>
2021-04-15 15:58:06 +0500

    Added the collection of names of letters in Hebre alphabet as the Resource to the XAML to show in the ListView. Every push of the button moves the selection in the ListView

--- ListViewProject/MainWindow.xaml.vb
+++ ListViewProject/MainWindow.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        hebrewAlphabet.SelectedIndex += 1
+        If hebrewAlphabet.SelectedIndex = hebrewAlphabet.Items.Count - 1 Then hebrewAlphabet.SelectedIndex = 0
 
+
+    End Sub
 End Class
